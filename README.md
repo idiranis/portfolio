81ab454d7531e09b8413eecc1a9482fcb7c8a249
Idir Anis <ai.anis.idir@gmail.com>
2021-03-22 13:04:24 +0100

    comit master

---
+++ README.md
@@ -0,0 +1,49 @@
+
+# anis idir portfolio
+## A clean, beautiful and responsive portfolio 
+
+## Features
+
+⚡️ Modern UI Design + Reveal Animations\
+⚡️ One Page Layout built with React\
+⚡️ Styled with Bootstrap v4.3 + Custom SCSS\
+⚡️ Fully Responsive\
+⚡️ Configurable color scheme\
+⚡️ Image optimization with Gatsby\
+⚡️ Easy site customization\
+⚡️ Well organized documentation
+
+
+---
+
+## Getting Started 🚀
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+### Prerequisites 📋
+
+You'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [NPM](http://npmjs.com)) installed on your computer.\
+Also you need to have installed [Gatsby CLI](https://www.gatsbyjs.org/docs/quick-start/)
+
+```
+node@v10.16.0 or higher
+npm@6.9.0 or higher
+git@2.17.1 or higher
+gatsby-cli@2.8.22 or higher
+```
+
+Also, you can use [Yarn](https://yarnpkg.com/) instead of NPM ☝️
+
+```
+yarn@v1.21.1 or higher
+```
+## Technologies used 🛠️
+
+- [Gatsby](https://www.gatsbyjs.org/) - Static Site Generator
+- [GraphQL](https://graphql.org/) - Query language for APIs
+- [React](https://es.reactjs.org/) - Front-End JavaScript library
+- [Bootstrap 4](https://getbootstrap.com/docs/4.3/getting-started/introduction/) - Front-End UI library
+- [Sass](https://sass-lang.com/documentation) - CSS extension language
+
+
+
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,229 @@
+import React, { useContext, useState, useEffect } from 'react';
+import Particles from 'react-tsparticles';
+import { Container } from 'react-bootstrap';
+import Fade from 'react-reveal/Fade';
+import { Link } from 'react-scroll';
+import { LiquidDistortionText } from 'react-text-fun';
+import PortfolioContext from '../../context/context';
+
+const Header = () => {
+  const { hero } = useContext(PortfolioContext);
+  const { title, subtitle, cta } = hero;
+
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+      setIsDesktop(false);
+    }
+  }, []);
+
+  return (
+    <section id="hero">
+      <Particles
+        id="tsparticles"
+        options={{
+          fpsLimit: 60,
+          interactivity: {
+            detectsOn: 'canvas',
+            events: {
+              onClick: {
+                enable: true,
+                mode: 'push',
+              },
+              onHover: {
+                enable: true,
+                mode: 'repulse',
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            links: {
+              enable: false,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: 'none',
+              enable: true,
+              outMode: 'bounce',
+              random: false,
+              speed: 6,
+              straight: false,
+            },
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+            },
+            opacity: {
+              value: 0.8,
+            },
+            shape: {
+              type: 'images',
+              image: [
+                {
+                  src: 'https://img.icons8.com/color/100/000000/github--v1.png',
+                  height: 100,
+                  width: 100,
+                },
+                {
+                  src: 'https://img.icons8.com/color/100/000000/html-5.png',
+                  height: 100,
+                  width: 100,
+                },
+                {
+                  src: 'https://img.icons8.com/color/100/000000/android-os.png',
+                  height: 100,
+                  width: 100,
+                },
+                {
+                  src: 'https://img.icons8.com/color/100/000000/css3.png',
+                  height: 100,
+                  width: 100,
+                },
+                {
+                  src: 'https://img.icons8.com/color/100/000000/javascript.png',
+                  height: 100,
+                  width: 100,
+                },
+                {
+                  src: 'https://img.icons8.com/color/100/000000/linux.png',
+                  height: 100,
+                  width: 100,
+                },
+                {
+                  src: 'https://img.icons8.com/color/50/000000/react-native.png',
+                  height: 100,
+                  width: 100,
+                },
+                {
+                  src: 'https://img.icons8.com/color/100/000000/mongodb.png',
+                  height: 100,
+                  width: 100,
+                },
+                {
+                  src: 'https://img.icons8.com/color/100/000000/mysql.png',
+                  height: 100,
+                  width: 100,
+                },
+                {
+                  src: 'https://img.icons8.com/color/100/000000/nodejs.png',
+                  height: 100,
+                  width: 100,
+                },
+                {
+                  src: 'https://img.icons8.com/color/100/000000/npm.png',
+                  height: 100,
+                  width: 100,
+                },
+                {
+                  src: 'https://img.icons8.com/officel/100/000000/react.png',
+                  height: 100,
+                  width: 100,
+                },
+                {
+                  src: 'https://img.icons8.com/color/100/000000/typescript.png',
+                  height: 100,
+                  width: 100,
+                },
+                {
+                  src: 'https://img.icons8.com/color/100/000000/symfony.png',
+                  height: 100,
+                  width: 100,
+                },
+                {
+                  src: 'https://img.icons8.com/color/100/000000/bootstrap.png',
+                  height: 100,
+                  width: 100,
+                },
+                {
+                  src: 'https://img.icons8.com/windows/100/000000/git-squared.png',
+                  height: 100,
+                  width: 100,
+                },
+                {
+                  src: 'https://img.icons8.com/color/100/000000/docker-container.png',
+                  height: 100,
+                  width: 100,
+                },
+                {
+                  src: 'https://img.icons8.com/color/100/000000/source-code.png',
+                  height: 100,
+                  width: 100,
+                },
+                {
+                  src: 'https://img.icons8.com/color/100/000000/material-ui.png',
+                  height: 100,
+                  width: 100,
+                },
+              ],
+            },
+            size: {
+              random: false,
+              value: 20,
+              anim: {
+                enable: true,
+                speed: 20,
+                size_min: 8,
+              },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+      <Container>
+        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
+          <h1 className="hero-title">
+            {title || '{  Hi, my name is'}{' '}
+            <LiquidDistortionText
+              className="hero-title"
+              text="Anis IDIR"
+              paddingTop={25}
+              fill="#0e0147"
+              fontSize={120}
+              speed={0.6}
+              volatility={0.02}
+            />
+            {subtitle || "I'm a Web & Mobile developer } ;"}
+          </h1>
+        </Fade>
+        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+          <p className="hero-cta">
+            <span className="cta-btn cta-btn--hero">
+              <Link to="about" smooth duration={1000}>
+                {cta || 'Know more'}
+              </Link>
+            </span>
+          </p>
+        </Fade>
+      </Container>
+    </section>
+  );
+};
+
+export default Header;
